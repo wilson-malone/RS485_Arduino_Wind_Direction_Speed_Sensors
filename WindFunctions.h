070c37848ae4fd21e1aaf00cf61d992024ba5cde
Wilson Malone <157308119+wilson-malone@users.noreply.github.com>
2024-06-23 16:57:14 -0400

    WindFunctions.h

--- WindFunctions.h
+++ WindFunctions.h
@@ -19,25 +19,6 @@ class WindFunctions{
 
 	
 	private:
-	//Variables
-	// uint8_t ModifyAddressCOM[11];
-	// uint8_t Data[7];
-	// uint8_t COM[8];
-	// boolean ret;
-	// long curr;
-	// long curr1;
-	// uint8_t ch;
-	// uint16_t crc;
-	// uint8_t *buf;
-	// int pos;
-	// int16_t Tineout;
-	// uint8_t *buffer;
-	// size_t left;
-	// size_t offset;
-
-
-	
-	
 	//Functions
 	size_t readN(uint8_t *buf, size_t len);
 	uint16_t CRC16_2(uint8_t *buf, int16_t len);
