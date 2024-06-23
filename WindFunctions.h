9842df30dce0b308c82b2f43c0443f37433c5bfa
Wilson Malone <157308119+wilson-malone@users.noreply.github.com>
2024-06-23 17:36:06 -0400

    WindFunctions.h

--- WindFunctions.h
+++ WindFunctions.h
@@ -8,14 +8,20 @@
 class WindFunctions{
 	public:
 	//Variables
+
 	int16_t WindDirection;
 	int16_t WindSpeed;
 	
 	//Functions
+
+	//Read Wind Speed
 	int16_t readWindSpeed(uint8_t A_Address);
-	int16_t readWindDirection16(uint8_t B_Address); //16 directions
-        int16_t readWindDirection360(uint8_t B_Address); //360 degrees, but you must divide by 10 to get the right number.
-	boolean ModifyAddress(uint8_t Address1, uint8_t Address2); //modifies the address of the connected unit. Only one at a time. Restart when done.
+	//Read Wind Direction as 16 directions
+	int16_t readWindDirection16(uint8_t B_Address);
+	//Read Wind direction as 360 degrees, but you must divide by 10 to get the right number.
+        int16_t readWindDirection360(uint8_t B_Address); 
+	//modifies the address of the connected unit. Only one at a time. Restart when done.
+	boolean ModifyAddress(uint8_t Address1, uint8_t Address2); 
 
 
 	
